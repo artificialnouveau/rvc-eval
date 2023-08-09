2c077148c57af41638708209306419bd22500c59
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-08-09 11:28:02 +0200

    Update cli.py

--- rvc_eval/cli.py
+++ rvc_eval/cli.py
@@ -1,8 +1,13 @@
+from pythonosc import udp_client
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
 import os
 import sys
 from argparse import ArgumentParser
 from logging import getLogger
-from scipy.io.wavfile import read, write'
+from scipy.io.wavfile import read, write
+from scipy.signal import resample_poly
 
 import soundfile as sf
 import numpy as np
@@ -15,9 +20,38 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../rvc/"))
 
 logger = getLogger(__name__)
 
+def print_handler(address, *args):
+    print(f"Received message from {address}: {args}")
+
+def receive_osc(unused_addr, model, input_file_path, output_file_path):
+    global args
+    args.model = model
+    args.input_file = input_file_path
+    args.output_file = output_file_path
+    main(args)
+
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/OSC_2py", print_handler)
+
+server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 1111), dispatcher)
+print("Serving on {}".format(server.server_address))
+server.serve_forever()
+
+def resample_audio(audio, original_sr, target_sr):
+    from math import gcd
+
+    # Calculate up and down sampling ratios
+    g = gcd(original_sr, target_sr)
+    up = target_sr // g
+    down = original_sr // g
+
+    return resample_poly(audio, up, down)
+
+
 def main(args):
     is_half = not args.float and args.device != "cpu"
     device = torch.device(args.device)
+    print('Device used: ', device)
 
     hubert_model = load_hubert(args.hubert, is_half, device)
     net_g, sampling_ratio = load_net_g(args.model, is_half, device)
@@ -29,61 +63,61 @@ def main(args):
     f0_method = args.f0_method
     vc = VC(sampling_ratio, device, is_half, repeat)
 
-    # Read input audio file parameters
-    input_frame_rate = 16000
-    frames_per_buffer = input_frame_rate * args.buffer_size // 1000
+    audio_output_chunks = []
 
-    # Placeholder to store the processed audio
-    audio_output_full = np.empty((0,))
+    with sf.SoundFile(args.input_file, 'r') as f:
+        input_frame_rate = f.samplerate
+        frames_per_buffer = input_frame_rate * args.buffer_size // 1000
+        
+        with tqdm(total=len(f), desc="Processing", unit="sample") as pbar:
+            while True:
+                audio_input = f.read(frames=frames_per_buffer)
 
-    total_iterations = len(sf.SoundFile(args.input_file)) // frames_per_buffer
-    
-    # Read the input audio file in chunks with a progress bar
-    with sf.SoundFile(args.input_file, 'r') as f, tqdm(total=total_iterations, desc="Processing", unit="chunk") as pbar:
-        while True:
-            audio_input = f.read(frames=frames_per_buffer)
-            
-            if len(audio_input) == 0:  # End of file
-                break
+                if len(audio_input) == 0:  # End of file
+                    break
 
-            # Check for multi-channel audio and convert to mono
-            if len(audio_input.shape) > 1 and audio_input.shape[1] > 1:
-                audio_input = np.mean(audio_input, axis=1)
-            
-            # Convert to float32
-            audio_input = audio_input.astype(np.float32)
+                if len(audio_input.shape) > 1 and audio_input.shape[1] > 1:
+                    audio_input = np.mean(audio_input, axis=1)
 
-            audio_output = (
-                vc.pipeline(
-                    hubert_model,
-                    net_g,
-                    sid,
-                    audio_input,  # don't pad here, leave it to the pipeline function
-                    f0_up_key,
-                    f0_method,
+                audio_input = audio_input.astype(np.float64)
+
+                audio_output = (
+                    vc.pipeline(
+                        hubert_model,
+                        net_g,
+                        sid,
+                        audio_input,
+                        f0_up_key,
+                        f0_method,
+                    )
                 )
-                .cpu()
-                .float()
-                .numpy()
-            )
 
-            # Trim the output to match the input's length
-            if len(audio_output) > len(audio_input):
-                audio_output = audio_output[:len(audio_input)]
+                audio_output_chunks.append(audio_output)
 
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
+                if torch.cuda.is_available():
+                    torch.cuda.empty_cache()
 
-            if np.isnan(audio_output).any():
-                continue
+                if np.isnan(audio_output).any():
+                    continue
 
-            audio_output_full = np.concatenate((audio_output_full, audio_output))
-            
-            # Increment the progress bar
-            pbar.update(1)
+                pbar.update(len(audio_input))
+    
+    audio_output_full = np.concatenate(audio_output_chunks)
 
-    # Write the processed audio to the output file using the original sample rate
-    sf.write(args.output_file, audio_output_full, input_frame_rate)
+    # Save the output
+    sf.write(args.output_file, audio_output_full.astype('float32'), 44100)  # Save at 44.1kHz rate
+        
+    # Specify the IP address of the remote host
+    remote_ip = "192.168.2.110"  # Replace with the IP address of the remote host
+
+    # Create a client to send OSC messages, targeting the remote host on port 5005
+    sender = udp_client.SimpleUDPClient(remote_ip, 6666)
+    # sender = udp_client.SimpleUDPClient("127.0.0.1", 6666)
+    sender.send_message("/OSC_2max", args.output_file)
+
+    # Send OSC command to Max to notify that file creation is complete
+    # client = udp_client.SimpleUDPClient('127.0.0.1', 6448)  # Assuming Max is listening on port 6448 at localhost
+    # client.send_message("/fileComplete", args.output_file)  # Sending the path as a message for confirmation
 
 
 parser = ArgumentParser()
@@ -99,7 +133,7 @@ parser.add_argument("-k", "--f0-up-key", type=int, default=0)
 parser.add_argument("--f0-method", type=str, default="pm", choices=("pm", "harvest"))
 parser.add_argument("--input-file", type=str, required=True, help="Path to input audio file")
 parser.add_argument("--output-file", type=str, required=True, help="Path to save processed audio file")
-parser.add_argument("--buffer-size", type=int, default=10000, help="buffering size in ms")
+parser.add_argument("--buffer-size", type=int, default=1000, help="buffering size in ms")
 
 
 if __name__ == "__main__":

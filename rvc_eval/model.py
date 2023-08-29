34077e6311d9daaf1616ad81389aeb18ae3f1c7f
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-08-29 17:47:45 +0800

    Update model.py

--- rvc_eval/model.py
+++ rvc_eval/model.py
@@ -19,5 +19,5 @@ def load_net_g(model_path: str, is_half: bool, device: torch.device):
     sampling_rate = cpt["config"][-1]
     net_g = SynthesizerTrnMs256NSFsid(*cpt["config"], is_half=is_half).to(device)
     net_g.eval()
-    net_g.load_state_dict(cpt["weight"], strict=False)
+    net_g.load_state_dict(cpt["weight"], strict=True)
     return (net_g.half() if is_half else net_g.float(), sampling_rate)

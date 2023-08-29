23612023e43318c2bd929bc079d3ab343d43f40f
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-08-29 17:40:57 +0800

    Update model.py

--- rvc_eval/model.py
+++ rvc_eval/model.py
@@ -15,7 +15,7 @@ def load_hubert(model_path: str, is_half: bool, device: torch.device):
 def load_net_g(model_path: str, is_half: bool, device: torch.device):
     from rvc.infer_pack.models import SynthesizerTrnMs256NSFsid
 
-    cpt = torch.load(model_path, map_location = device)
+    cpt = torch.load(model_path, map_location="cpu")
     sampling_rate = cpt["config"][-1]
     net_g = SynthesizerTrnMs256NSFsid(*cpt["config"], is_half=is_half).to(device)
     net_g.eval()
